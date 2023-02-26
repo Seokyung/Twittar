cd72632c4a4c8ee4203b018df344e5f29380fcd3
Seokyung <xuejd17@gmail.com>
2023-02-27 00:21:04 +0900

    Add .env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBKki03fW6uqEoZ9zcfrjEj7Mp5zs1X3_A",
-  authDomain: "twittar-12a71.firebaseapp.com",
-  projectId: "twittar-12a71",
-  storageBucket: "twittar-12a71.appspot.com",
-  messagingSenderId: "622693674085",
-  appId: "1:622693674085:web:b71a1d767a4b0b6765aa8c"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
 };
 
 export const app = initializeApp(firebaseConfig);
