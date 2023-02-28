3e5b6dfc85028edca90856349af7ca196ba8be6f
Seokyung <xuejd17@gmail.com>
2023-02-28 18:56:22 +0900

    Setup Navigation

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,22 +1,26 @@
 import React from "react";
-import {HashRouter as Router, Route, Routes} from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Navigation from "./Navigation";
 import Home from "../routes/Home";
 import Auth from "../routes/Auth";
+import Profile from "routes/Profile";
 
-function AppRouter({isLoggedIn}) {
-    return(
-        <Router>
-            <Routes>
-                {isLoggedIn ? (
-                    <>
-                        <Route path="/" element={<Home/>}/>
-                    </>
-                    ) : (
-                    <Route path="/" element={<Auth/>}/>
-                )}
-            </Routes>
-        </Router>
-    );
+function AppRouter({ isLoggedIn }) {
+	return (
+		<Router>
+			{isLoggedIn && <Navigation />}
+			<Routes>
+				{isLoggedIn ? (
+					<>
+						<Route path="/" element={<Home />} />
+						<Route path="/profile" element={<Profile />} />
+					</>
+				) : (
+					<Route path="/" element={<Auth />} />
+				)}
+			</Routes>
+		</Router>
+	);
 }
 
 export default AppRouter;
