915b8951c07ce1788da7989f21028a7169dc4008
Seokyung <xuejd17@gmail.com>
2023-03-01 19:31:08 +0900

    Setup Twitt Form

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+function Home() {
+	const [twitt, setTwitt] = useState("");
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+	};
+
+	const onChange = (e) => {
+		const {
+			target: { value },
+		} = e;
+		setTwitt(value);
+	};
+
+	return (
+		<div>
+			<form onSubmit={onSubmit}>
+				<input
+					value={twitt}
+					onChange={onChange}
+					type="text"
+					placeholder="What's on your mind?"
+					maxLength={200}
+				/>
+				<input type="submit" value="twitt" />
+			</form>
+		</div>
+	);
+}
 
 export default Home;
