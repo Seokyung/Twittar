c8d0ca7fc4e571dd19945c88e51eb76c0589f730
Seokyung <xuejd17@gmail.com>
2023-03-01 20:46:54 +0900

    Implement Create, Read Twitts

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,18 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { dbService } from "fbase";
-import { addDoc, collection } from "firebase/firestore";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
 function Home() {
 	const [twitt, setTwitt] = useState("");
+	const [twitts, setTwitts] = useState([]);
+
+	const getTwitts = async () => {
+		const dbTwitts = await getDocs(collection(dbService, "twitts"));
+		dbTwitts.forEach((doc) => {
+			const twittObject = {
+				...doc.data(),
+				id: doc.id,
+			};
+			setTwitts((prev) => [twittObject, ...prev]);
+		});
+	};
+
+	useEffect(() => {
+		getTwitts();
+	}, []);
 
 	const onSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const docRef = await addDoc(collection(dbService, "twitts"), {
+			await addDoc(collection(dbService, "twitts"), {
 				twitt,
 				createdAt: Date.now(),
 			});
-			console.log("Document written with ID: ", docRef.id);
 		} catch (error) {
 			alert(error);
 			console.log("Error adding document");
@@ -39,6 +54,13 @@ function Home() {
 				/>
 				<input type="submit" value="twitt" />
 			</form>
+			<div>
+				{twitts.reverse().map((twitt) => (
+					<div key={twitt.id}>
+						<h4>{twitt.twitt}</h4>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
