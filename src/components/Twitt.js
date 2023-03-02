2ae2f646c778337391d17b18d28ce37b2a33383c
Seokyung <xuejd17@gmail.com>
2023-03-02 16:46:23 +0900

    Implement Edit Twitt

--- src/components/Twitt.js
+++ src/components/Twitt.js
@@ -1,8 +1,36 @@
-import React from "react";
-import { doc, deleteDoc } from "firebase/firestore";
+import React, { useEffect, useRef, useState } from "react";
+import { doc, deleteDoc, updateDoc } from "firebase/firestore";
 import { dbService } from "fbase";
 
 function Twitt({ twittObj, isOwner }) {
+	const [isEdit, setIsEdit] = useState(false);
+	const [newTwitt, setNewTwitt] = useState(twittObj.text);
+	const editFocus = useRef(null);
+
+	useEffect(() => {
+		if (isEdit) {
+			editFocus.current.focus();
+		}
+	}, [isEdit]);
+
+	const toggleEdit = () => {
+		setIsEdit((prev) => !prev);
+	};
+
+	const onEditChange = (e) => {
+		const {
+			target: { value },
+		} = e;
+		setNewTwitt(value);
+	};
+
+	const onEditClick = async (e) => {
+		e.preventDefault();
+		const twittDocRef = doc(dbService, "twitts", `${twittObj.id}`);
+		await updateDoc(twittDocRef, { text: newTwitt });
+		setIsEdit(false);
+	};
+
 	const onDeleteClick = async () => {
 		const isDelete = window.confirm("Are you sure to delete this twitt?");
 		if (isDelete) {
@@ -13,11 +41,30 @@ function Twitt({ twittObj, isOwner }) {
 
 	return (
 		<div>
-			<h4>{twittObj.text}</h4>
-			{isOwner && (
+			{isEdit ? (
 				<>
-					<button>Edit Twitt</button>
-					<button onClick={onDeleteClick}>Delete Twitt</button>
+					<form onSubmit={onEditClick}>
+						<input
+							type="text"
+							placeholder="Edit your twitt!"
+							onChange={onEditChange}
+							ref={editFocus}
+							value={newTwitt}
+							required
+						/>
+						<input type="submit" value="Update twitt" />
+					</form>
+					<button onClick={toggleEdit}>Cancel</button>
+				</>
+			) : (
+				<>
+					<h4>{twittObj.text}</h4>
+					{isOwner && (
+						<>
+							<button onClick={toggleEdit}>Edit twitt</button>
+							<button onClick={onDeleteClick}>Delete twitt</button>
+						</>
+					)}
 				</>
 			)}
 		</div>
