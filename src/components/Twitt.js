07bef690099fab6b4e8ffc197efee5ba4ec3caf5
Seokyung <xuejd17@gmail.com>
2023-03-02 16:13:16 +0900

    Implement Delete Twitt

--- src/components/Twitt.js
+++ src/components/Twitt.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { doc, deleteDoc } from "firebase/firestore";
+import { dbService } from "fbase";
 
 function Twitt({ twittObj, isOwner }) {
+	const onDeleteClick = async () => {
+		const isDelete = window.confirm("Are you sure to delete this twitt?");
+		if (isDelete) {
+			const twittDocRef = doc(dbService, "twitts", `${twittObj.id}`);
+			await deleteDoc(twittDocRef);
+		}
+	};
+
 	return (
 		<div>
 			<h4>{twittObj.text}</h4>
 			{isOwner && (
 				<>
 					<button>Edit Twitt</button>
-					<button>Delete Twitt</button>
+					<button onClick={onDeleteClick}>Delete Twitt</button>
 				</>
 			)}
 		</div>
